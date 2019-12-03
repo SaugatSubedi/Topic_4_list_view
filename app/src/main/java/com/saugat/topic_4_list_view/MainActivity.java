4ce1ca636667b500d0f8096ca4f4350d4e7e7af4
Saugat Subedi <subedisaugat176@gmail.com>
2019-12-03 07:20:32 +0545

    Initial commit

---
+++ app/src/main/java/com/saugat/topic_4_list_view/MainActivity.java
@@ -0,0 +1,60 @@
+package com.saugat.topic_4_list_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String countries[] = {
+
+            "Nepal", "Kathmandu",
+            "India", "New Delhi",
+            "China", "Beijing",
+            "UK",    "London",
+            "USA",    "Washington, D.C.",
+            "Canada", "ottawa"
+    };
+    private Map<String,String> dictionary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lstCountries = findViewById(R.id.lstCountries);
+
+        dictionary =new HashMap<>();
+        for (int i=0; i<countries.length; i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(
+                this, //activity
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(dictionary.keySet())
+        );
+        lstCountries.setAdapter(countryAdapter);
+
+        lstCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String country = parent.getItemAtPosition(position).toString();
+                String capital = dictionary.get(country);
+                //Toast.makeText(getApplicationContext(), capital.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/saugat/topic_4_list_view/empty_activity.java
@@ -0,0 +1,15 @@
+package com.saugat.topic_4_list_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class empty_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_activity);
+    }
+}
+fb
